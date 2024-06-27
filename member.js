621c642e66cba5b2f19f6d75eebdd179c16d8620
Dom Eales @ PaloIT <93354893+domeales-paloit@users.noreply.github.com>
2024-06-27 07:58:59 +0000

    Copilot 2nd commit

---
+++ member.js
@@ -0,0 +1,30 @@
+function skillsMember() {
+	// Skills
+	var skills = {
+		"Strength": 1,
+		"Agility": 1,
+		"Smithing": 1,
+		"Defence": 1,
+		"Herblore": 1,
+		"Fishing": 1,
+		"Ranged": 1,
+		"Thieving": 1,
+		"Cooking": 1,
+		"Prayer": 1,
+		"Crafting": 1,
+		"Firemaking": 1,
+		"Magic": 1,
+		"Fletching": 1,
+		"Woodcutting": 1,
+		"Runecrafting": 1,
+		"Slayer": 1,
+		"Farming": 1,
+		"Construction": 1,
+		"Hunter": 1,
+		"Summoning": 1,
+		"Dungeoneering": 1,
+		"Divination": 1,
+		"Invention": 1
+	};
+	return skills;
+}
