e0ab4cdf5f6692f5c3eeb2beb9d4cfd9983647bd
Dom Eales @ PaloIT <93354893+domeales-paloit@users.noreply.github.com>
2024-06-27 08:14:11 +0000

    Copilot 3rd commit

--- comments.js
+++ comments.js
@@ -1,73 +1,14 @@
 // Create a web server
-// Create a route for GET /comments
-// Create a route for POST /comments
-// Create a route for DELETE /comments
-// Create a route for PUT /comments
-
 const express = require('express');
 const app = express();
-const comments = require('./comments.json');
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const port = 3000;
 
-app.use(bodyParser.json());
-
+// Create a route and send a response
 app.get('/comments', (req, res) => {
-    res.json(comments);
+  res.send('This is a GET request to /comments');
 });
 
-app.post('/comments', (req, res) => {
-    const newComment = req.body;
-    newComment.id = comments.length + 1;
-    comments.push(newComment);
-
-    fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
-        if (err) {
-            res.status(500).json({ message: 'An error has occurred while writing a new comment' });
-        } else {
-            res.json(newComment);
-        }
-    });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
 });
-
-app.delete('/comments/:id', (req, res) => {
-    const commentId = parseInt(req.params.id);
-    const commentIndex = comments.findIndex((comment) => comment.id === commentId);
-
-    if (commentIndex > -1) {
-        comments.splice(commentIndex, 1);
-
-        fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
-            if (err) {
-                res.status(500).json({ message: 'An error has occurred while deleting the comment' });
-            } else {
-                res.json({ message: 'Comment has been deleted' });
-            }
-        });
-    } else {
-        res.status(404).json({ message: 'Comment not found' });
-    }
-});
-
-app.put('/comments/:id', (req, res) => {
-    const commentId = parseInt(req.params.id);
-    const commentIndex = comments.findIndex((comment) => comment.id === commentId);
-
-    if (commentIndex > -1) {
-        const updatedComment = { ...comments[commentIndex], ...req.body };
-
-        comments[commentIndex] = updatedComment;
-
-        fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
-            if (err) {
-                res.status(500).json({ message: 'An error has occurred while updating the comment' });
-            } else {
-                res.json(updatedComment);
-            }
-        });
-    } else {
-        res.status(404).json({ message: 'Comment not found' });
-    }
-});
-
-app
